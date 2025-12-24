08fc761665fbff28600d93835a8429cd3f5f5cb2
Jangchaeyun <zzangsally@naver.com>
2025-12-24 22:05:09 +0900

    1224 랜딩 페이지 섹션 추가 (CTA, 기능, 푸터, 히어로 페이지, 가격, 고객 후기)

---
+++ cloudsharewebapp/src/components/landing/HeroSection.jsx
@@ -0,0 +1,14 @@
+const HeroSection = () => {
+  return (
+    <div className="landing-page-content relative">
+      <div className="absolute inset-0 bg-gradient-to-r from-purple-50 to-indigo-50 opacity-80 z-0 pointer-events-none"></div>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
+        <div className="pt-20 pb-16 sm:pt-24 sm:pb-20 lg:pt-32 lg:pb-28 "></div>
+        <div className="relative"></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
--- cloudsharewebapp/src/pages/Landing.jsx
+++ cloudsharewebapp/src/pages/Landing.jsx
@@ -1,6 +1,26 @@
+import CTASection from "../components/landing/CTASection";
+import FeaturesSection from "../components/landing/FeaturesSection";
+import FooterSection from "../components/landing/FooterSection";
+import HeroSection from "../components/landing/HeroSection";
+import PricingSection from "../components/landing/PricingSection";
+import TestimonialsSection from "../components/landing/TestimonialsSection";
+
 const Landing = () => {
   return (
-    <div className="landing-page bg-gradient-to-b from-gray-50to-gray-100"></div>
+    <div className="landing-page bg-gradient-to-b from-gray-50to-gray-100">
+      {/* Hero Section */}
+      <HeroSection />
+      {/* Feature section */}
+      <FeaturesSection />
+      {/* Pricing section */}
+      <PricingSection />
+      {/* Testimonials section */}
+      <TestimonialsSection />
+      {/* CTA section */}
+      <CTASection />
+      {/* Footer section */}
+      <FooterSection />
+    </div>
   );
 };
 
