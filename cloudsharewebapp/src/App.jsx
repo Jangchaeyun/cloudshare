f4f012ff132feef9e3075194f50379e5a9070ff5
Jangchaeyun <zzangsally@naver.com>
2025-12-30 21:53:24 +0900

    1230 Clerk 통합 및 인증 기능 추가

--- cloudsharewebapp/src/App.jsx
+++ cloudsharewebapp/src/App.jsx
@@ -5,13 +5,31 @@ import Upload from "./pages/Upload";
 import MyFiles from "./pages/MyFiles";
 import Subscription from "./pages/Subscription";
 import Transactions from "./pages/Transactions";
+import {
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+  SignIn,
+} from "@clerk/clerk-react";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/dasboard" element={<Dashboard />} />
+        <Route
+          path="/dasboard"
+          element={
+            <>
+              <SignedIn>
+                <Dashboard />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          }
+        />
         <Route path="/upload" element={<Upload />} />
         <Route path="/my-files" element={<MyFiles />} />
         <Route path="/subscriptions" element={<Subscription />} />
--- cloudsharewebapp/src/pages/Landing.jsx
+++ cloudsharewebapp/src/pages/Landing.jsx
@@ -1,3 +1,4 @@
+import { useClerk, useUser } from "@clerk/clerk-react";
 import { features, pricingPlans } from "../assets/data";
 import CTASection from "../components/landing/CTASection";
 import FeaturesSection from "../components/landing/FeaturesSection";
@@ -5,12 +6,23 @@ import FooterSection from "../components/landing/FooterSection";
 import HeroSection from "../components/landing/HeroSection";
 import PricingSection from "../components/landing/PricingSection";
 import TestimonialsSection from "../components/landing/TestimonialsSection";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Landing = () => {
+  const { openSignIn, openSignUp } = useClerk();
+  const { isSignedIn } = useUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isSignedIn) {
+      navigate("/dashboard");
+    }
+  }, [isSignedIn, naviage]);
   return (
     <div className="landing-page bg-gradient-to-b from-gray-50to-gray-100">
       {/* Hero Section */}
-      <HeroSection />
+      <HeroSection openSignIn={openSignIn} openSignUp={openSignUp} />
       {/* Feature section */}
       <FeaturesSection features={features} />
       {/* Pricing section */}
