5ed3cbf4c703df89e003fd20fa2aefde95a728eb
Jangchaeyun <zzangsally@naver.com>
2025-12-30 20:42:40 +0900

    1230 CTASection 스타일 개선 및 FooterSection 구현

--- cloudsharewebapp/src/components/landing/CTASection.jsx
+++ cloudsharewebapp/src/components/landing/CTASection.jsx
@@ -1,10 +1,10 @@
 const CTASection = () => {
   return (
     <div className="bg-purple-500">
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center">
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
         <h2 className="text-3xl font-extrabold tracking-tight text-white sm:text-4xl">
           <span className="block">시작할 준비 되셨나요?</span>
-          <span className="block text-purple">계정을 오늘 생성하세요.</span>
+          <span className="block text-purple-100">계정을 오늘 생성하세요.</span>
         </h2>
         <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
           <div className="inline-flex rounded-md shadow">
